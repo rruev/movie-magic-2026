a62f709b594e13535e5759f21cfd1388b0a287cc
rruev <rosenruev@gmail.com>
2026-07-01 11:53:14 +0200

    set up debugg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('hbs', engine({ extname: '.hbs' }));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Movie Magic 2026' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
