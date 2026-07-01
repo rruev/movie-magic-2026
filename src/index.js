906494f24378e0b191aee993d4ffb30fbd0be5b5
rruev <rosenruev@gmail.com>
2026-07-01 12:37:37 +0200

    Set up layout

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import { engine } from 'express-handlebars';
 
 const app = express();
 
+// Set up handlebars as the view engine
 app.engine('hbs', engine({ extname: '.hbs' }));
-
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+// Set up static files serving
+app.use(express.static('src/public'));
+
+// Define routes
 app.get('/', (req, res) => {
   res.render('home', { title: 'Movie Magic 2026' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About Movie Magic 2026' });
 });
 
 app.listen(3000, () => {
