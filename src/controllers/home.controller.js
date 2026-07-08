912cdcfcebbf961e396230de06f35d6ff869c673
rruev <rosenruev@gmail.com>
2026-07-08 10:39:51 +0200

    show static search page

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -13,4 +13,8 @@ homeController.get('/about', (req, res) => {
     res.render('about', { title: 'About Movie Magic 2026' });
 });
 
+homeController.get('/search', (req, res) => {
+    res.render('search', { title: 'Search Movies' });
+});
+
 export default homeController;
